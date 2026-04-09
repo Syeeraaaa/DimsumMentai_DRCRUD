a1c4f68f47ebb6e73868e205d6f35404cbe76c34
Syeeraaaa <syeerasilvia@gmai.com>
2026-04-09 17:37:39 +0700

    Menambahkan event tombol koenksi untuk menghubungkan ke database

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -48,5 +48,10 @@ namespace CRUDMahasiswaADO
         {
 
         }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            ConnectDatabase();
+        }
     }
 }
